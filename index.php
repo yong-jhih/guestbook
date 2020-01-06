a69e68048e2370683dac86d807ead77e3a4d0c97
yong-jhih <waityouforever77@gmail.com>
2020-01-06 18:15:50 +0800

    use bootstrap beautify index.login.register pages

--- index.php
+++ index.php
@@ -1,67 +1,106 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>guestbook</title>
-    <script src="guestbook.js"></script>
+    <script src="jquery-3.4.1.js"></script>
+    <link rel="stylesheet" href="bootstrap-4.4.1-dist\css\bootstrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
 </head>
+
 <body>
     <?php include 'statusBar.php' ?>
-    <h1 align="center">留言版</h1>
-    <hr>
-    
+    <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">會員登入</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="checkpwd.php">
+                        <div class="form-group row">
+                            <label for="staticEmail" class="col-sm-2 col-form-label">帳號</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="staticEmail" name="memberAC" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="inputPassword" class="col-sm-2 col-form-label">密碼</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="inputPassword" name="memberPW" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">關閉</button>
+                            <button type="submit" class="btn btn-primary">登入</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
     <?php
-        // 資料處理
-        require_once("db_connect.php");
-        $recordPerPage = 5 ;
-        if(isset($_GET["page"]))
-            $page = $_GET["page"];
-        else
-            $page = 1 ;
-            
-        $database = "guestbook";
-        $link = create_connection();
-        $sql = "SELECT * FROM message ORDER BY id DESC";
-        $result = execute_db($link,"guestbook",$sql);
-        $totalRecords = mysqli_num_rows($result);
-        $totalPages = ceil($totalRecords/$recordPerPage);
-        $startedRecord = $recordPerPage * ($page - 1);
-        mysqli_data_seek($result, $startedRecord);
+    // 資料處理
+    require_once("db_connect.php");
+    $recordPerPage = 5;
+    if (isset($_GET["page"]))
+        $page = $_GET["page"];
+    else
+        $page = 1;
 
-        // 繪製表格
-        echo "<table width='800' align='center' cellspacing='3'>";
-            $j = 1;
-            while ($row = mysqli_fetch_assoc($result) and $j <= $recordPerPage){
-                echo "<tr>";
-                echo "<td>作者：" . $row["author"] . "<br>";
-                echo "主題：" . $row["subject"] . "<br>";
-                echo "時間：" . $row["date"] . "<hr>";
-                echo $row["content"] . "</td></tr>";
-                $j++;
-            }
-        echo "</table>" ;
-        
-        // 跳頁
-        echo "<hr>";
-        echo "<p align='center'>";
-            if ($page > 1){
-                echo "<a href='index.php?page=". ($page - 1) . "'>上一頁</a> ";
-            }
+    $database = "guestbook";
+    $link = create_connection();
+    $sql = "SELECT * FROM message ORDER BY id DESC";
+    $result = execute_db($link, "guestbook", $sql);
+    $totalRecords = mysqli_num_rows($result);
+    $totalPages = ceil($totalRecords / $recordPerPage);
+    $startedRecord = $recordPerPage * ($page - 1);
+    mysqli_data_seek($result, $startedRecord);
 
-            for ($i = 1; $i <= $totalPages; $i++){
-                if ($i == $page){
-                    echo "$i ";
-                }else{
-                    echo "<a href='index.php?page=$i'>$i</a> ";
-                } 
-            }
-            
-            if ($page < $totalPages){
-                echo "<a href='index.php?page=". ($page + 1) . "'>下一頁</a> ";
-            }
-        echo "</p>";
+    // 繪製表格
+    echo "<table width='800' align='center' cellspacing='3'>";
+    $j = 1;
+    while ($row = mysqli_fetch_assoc($result) and $j <= $recordPerPage) {
+        echo "<tr>";
+        echo "<td>作者：" . $row["author"] . "<br>";
+        echo "主題：" . $row["subject"] . "<br>";
+        echo "時間：" . $row["date"] . "<hr>";
+        echo $row["content"] . "</td></tr>";
+        $j++;
+    }
+    echo "</table>";
+
+    // 跳頁
+    echo "<hr>";
+    echo "<p align='center'>";
+    if ($page > 1) {
+        echo "<a href='index.php?page=" . ($page - 1) . "'>上一頁</a> ";
+    }
+
+    for ($i = 1; $i <= $totalPages; $i++) {
+        if ($i == $page) {
+            echo "$i ";
+        } else {
+            echo "<a href='index.php?page=$i'>$i</a> ";
+        }
+    }
+
+    if ($page < $totalPages) {
+        echo "<a href='index.php?page=" . ($page + 1) . "'>下一頁</a> ";
+    }
+    echo "</p>";
     ?>
 
     <!-- 填寫表單 -->
@@ -74,13 +113,13 @@
             </tr>
             <tr bgcolor="#D9F2FF">
                 <td width="15%">作者</td>
-            <?php
-                if(isset($_COOKIE['memberName'])){
-                    echo "<td width='85%'><input name='author' readonly type='text' size='50' value='".$memberName."'></td>";
-                }else{
+                <?php
+                if (isset($_COOKIE['memberName'])) {
+                    echo "<td width='85%'><input name='author' readonly type='text' size='50' value='" . $memberName . "'></td>";
+                } else {
                     echo '<td width="85%"><input name="author" type="text" size="50"></td>';
                 }
-            ?>
+                ?>
             </tr>
             <tr bgcolor="#84D7FF">
                 <td width="15%">主題</td>
@@ -92,11 +131,26 @@
             </tr>
             <tr>
                 <td colspan="2" align="center">
-                    <input type="button" value="張貼留言" onClick="checkForm()">　
-                    <input type="reset" value="重新輸入">
+                    <input type="button" value="張貼留言" class='btn btn-outline-primary' onClick="checkForm()">　
+                    <input type="reset" class='btn btn-outline-primary' value="重新輸入">
                 </td>
             </tr>
         </table>
     </form>
+
+    <script>
+        function checkForm() {
+            if (document.myForm.author.value.length == 0) {
+                alert("請留名字");
+            } else if (document.myForm.subject.value.length == 0) {
+                alert("請給主題");
+            } else if (document.myForm.content.value.length == 0) {
+                alert("請給內容");
+            } else {
+                myForm.submit();
+            }
+        }
+    </script>
 </body>
+
 </html>
--- statusBar.php
+++ statusBar.php
@@ -1,21 +1,63 @@
-<span><a href="index.php"><img src="snapshot.jpg" style="width: 50px;height:50px"></a>
-<?php if(isset($_COOKIE['passed'])){
-    $memberName = $_COOKIE['memberName'];
-    echo $_COOKIE['memberName'] . " 您好 歡迎回來 "."<div style='text-align:right;margin-right:5px'><button onclick='logOut()' style='margin-right:5px'>登出</button><button onclick='redirectMember()'>會員中心</button></div>";
-}else{
-    echo "<div style='text-align:right'><button style='margin-right:5px' onclick='redirectLogIn()'>登入</button><button onclick='redirectRegister()'>註冊</button></div>";
-} ?></span>
-<script>
-    function redirectLogIn(){
-        window.location = "login.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="jquery-3.4.1.js"></script>
+  <link rel="stylesheet" href="bootstrap-4.4.1-dist\css\bootstrap.css">
+  <script src="bootstrap-4.4.1-dist\js\bootstrap.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+      aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="index.php">留言板</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">
+          <?php
+            if(isset($_COOKIE['passed'])){
+              $memberName = $_COOKIE['memberName'];
+              echo $memberName . " 歡迎您回來." ;
+            }
+          ?>
+        </a>
+      </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style='margin-right:5px'>Search</button>
+      </form>
+      <?php
+        if(isset($_COOKIE['passed'])){
+          echo "<div style='text-align:right;margin-right:5px'><button class='btn btn-outline-primary' onclick='logOut()' style='margin-right:5px'>登出</button><button class='btn btn-outline-primary' onclick='redirectMember()'>會員中心</button></div>";
+            }else{
+              echo "<div style='text-align:right'><button class='btn btn-outline-primary' style='margin-right:5px' data-toggle='modal' data-target='#staticBackdrop'>登入</button><button class='btn btn-outline-primary' onclick='redirectRegister()'>註冊</button></div>";
+            }  
+    ?>
+    </div>
+  </nav>
+  <script>
+    function redirectRegister() {
+      window.location = "register.php";
     }
-    function redirectRegister(){
-        window.location = "register.php";
+
+    function redirectMember() {
+      window.location = "member.php";
     }
-    function redirectMember(){
-        window.location = "member.php";
+
+    function logOut() {
+      window.location = "logout.php";
     }
-    function logOut(){
-        window.location = "logout.php";
-    }
-</script>
+  </script>
+</body>
+
+</html>
